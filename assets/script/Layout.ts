83bcaedb8708d0a8eb1762beed190dd97fcee3e3
blakeyi <1152420971@qq.com>
2022-09-29 00:51:11 +0800

    chore:完善布局

--- assets/script/Layout.ts
+++ assets/script/Layout.ts
@@ -1,5 +1,21 @@
-import { _decorator, Component, Node, Sprite, Prefab, instantiate, Vec3, log, Button } from 'cc';
+import { _decorator, Component, Node, Sprite, Prefab, instantiate, Vec3, log, Button, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
+
+
+
+interface itemNode {
+    node : Node
+    postion : Vec3,
+    id : number
+}
+
+interface eventData {
+    id : number,
+    itemIndex :number
+}
+
+const ITEM_WIDTH = 50
+const ITEM_HEIGHT = 55
 
 @ccclass('Layout')
 export class Layout extends Component {
@@ -16,8 +32,8 @@ export class Layout extends Component {
     @property({type:Number})
     private layoutWidth = 450
 
-    @property({type:Number})
-    private layoutHeight = 300
+    private itemList:itemNode[] = []
+
 
     start() {
 
@@ -26,34 +42,89 @@ export class Layout extends Component {
     onLoad() {
         let oriPos = this.node.getPosition();
         log(oriPos)
-        const ITEM_WIDTH = 50, ITEM_HEIGHT = 55
-        const ROW_NUM = this.layoutHeight / ITEM_HEIGHT / 2
-        const COL_NUM = this.layoutWidth / ITEM_WIDTH / 2
-
-        let arr = this.getPosition(this.itemNum, ROW_NUM, COL_NUM)
-        log(arr)
+        const ROW_NUM = this.layoutWidth / ITEM_WIDTH
+        let posArr = this.getPosArray(ROW_NUM, ITEM_WIDTH, ITEM_HEIGHT)
+        log(posArr)
+        const arrLen = posArr.length
         for (let i = 0; i < this.itemNum; i++) {
-            const curPos = arr[i]
             let itemIndex:number =  i % 3
             let tempNode:Node = instantiate(this.items[itemIndex])
-            let vec = new Vec3(oriPos.x + curPos.x * ITEM_WIDTH / 2, oriPos.y + curPos.y  * ITEM_HEIGHT / 2, oriPos.z)
-            log(vec)
-            tempNode.setPosition(vec)
-            this.addClickEvent(tempNode, itemIndex)
-            // tempNode.getComponent(Sprite).grayscale = true
+            // 随机产生一个值在 0 到 arrLen 中的数
+            let posIndex = Math.floor(Math.random() * arrLen) 
+            tempNode.setPosition(posArr[posIndex])
+            this.addClickEvent(tempNode, {
+                itemIndex:itemIndex,
+                id:i
+            })
+
+            this.itemList.push({
+                node:tempNode,
+                postion:posArr[posIndex],
+                id:i
+            })
             this.node.addChild(tempNode)
+
         }
-        
+        // 计算是否被遮挡了
+        this.setGrayNode(ITEM_WIDTH, ITEM_HEIGHT)
+                
     }
 
-    addClickEvent(node:Node, itemIndex:number) {
+    setGrayNode(itemWidth:number, itemHeight:number) {
+        // 从后往前遍历, 因为最后添加的都在上面, 所以检查上面的区域是否被占了就ok
+        let posMap = new Map()
+        for (let i = this.itemList.length - 1; i >= 0; i--) {
+            const item = this.itemList[i]
+            // 检查是否有中心点重复的
+            let replicate = false
+            if (posMap.has(item.postion)) {
+                replicate = true
+                item.node.getComponent(Sprite).grayscale = true
+            } else {
+                // 遍历检查是否有重叠部分的
+                posMap.forEach((_, key) => {
+                    if (this.isReplicate(key, item.postion, itemWidth, itemHeight)) {
+                        replicate = true
+                        item.node.getComponent(Sprite).grayscale = true
+                    }
+                })
+                posMap.set(item.postion, true)
+            }
+            if (!replicate) {
+                item.node.getComponent(Sprite).grayscale = false
+            }
+        }
+    }
+
+    // 检查是否重叠, 通过判断中心点的距离
+    isReplicate( vec1: Vec3, vec2: Vec3, itemWidth:number, itemHeight:number) {
+        return Math.abs(vec1.x - vec2.x) < itemWidth &&  Math.abs(vec1.y - vec2.y) < itemHeight
+    }
+
+    // 根据行数获取所有能放的位置
+    getPosArray(rowNum : number, itemWidth:number, itemHeight:number) :Vec3[] {
+        let res = []
+        let length = 2 * rowNum - 1
+        let mid = Math.floor(length / 2)
+        for (let i= 0; i < length; i++) {
+            // 每一行的高度是一样的
+            let y = (mid - i) * itemHeight / 2
+            for (let j= 0; j < length; j++) {
+                let x = (j - mid) * itemWidth / 2 // 需要除2,可以重叠一半
+                res.push(new Vec3(x, y, 0))
+            }
+        }
+        return res
+    }
+
+    addClickEvent(node:Node, customEventData: eventData) {
         // log(node)
         let button= node.getComponent(Button);
 		let eventHandler=new Component.EventHandler();
 		eventHandler.target= this.node;
 		eventHandler.component="Layout"; // 界面上不会显示绑定上了
 		eventHandler.handler="onItemClick";
-		eventHandler.customEventData=itemIndex.toString();
+		eventHandler.customEventData=JSON.stringify(customEventData)
 		
 		button.clickEvents.push(eventHandler);
     }
@@ -76,7 +147,7 @@ export class Layout extends Component {
         
     }
 
-    onItemClick(event, customEventData) {
+    onItemClick(event, customEventData:string) {
         let curNode = event.currentTarget
         log("点击了", curNode.getPosition())
         let name = curNode.name
@@ -85,7 +156,15 @@ export class Layout extends Component {
         if (!status) {
             // 计算当前摆放位置坐标
             //this.storeNode.addChild(curNode)
-            this.calcPosInStore(parseInt(customEventData))
+            let data:eventData = JSON.parse(customEventData)
+            this.calcPosInStore(data.itemIndex)
+
+            // 从node里删除
+            this.node.removeChild(curNode)
+            // 从itemList中删除
+            this.itemList = this.itemList.filter(item => item.id !== data.id)
+            // 刷新状态
+            this.setGrayNode(ITEM_WIDTH, ITEM_HEIGHT)
         }
     }
 
